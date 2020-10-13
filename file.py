8ac1daefc6ce6e361880aea588b6388bd7ba2a94
Manikandan Nair <mani4pmn@gmail.com>
2020-10-13 07:31:54 +0000

    Add files via upload

--- file.py
+++ file.py
@@ -1,25 +1,18 @@
 import numpy as np
 from PIL import Image
-import math
+import skimage
 
-def resize():
-    image=Image.open('abc.jpg')
-    image=image.resize([512,512])
-    array=np.array(image)
+
+def resize(file_name)
+    image=Image.open(file_name)
+    dimension=512
+    height=image.size[0]
+    width=image.size[1]
+    image=image.resize([dimension,dimension])
+    no_of_blocks=(dimension*dimension)/(8*8)
+    max_limit=(no_of_blocks)**(1/2)
+    array=np.asarray(image)
     return array
 
-def find_average(array,limit):
-    averaged_array=[]
-    for i in range(0,limit,2):
-        average=(array[i]+array[i+1])/2
-        averaged_array.append(average)
-    return averaged_array
-
-def find_difference(averaged_array, original_array):
-    limit=int(len(original_array)/2)
-    for i in range(0,limit):
-        difference=original_array[i]-averaged_array[i]
-        averaged_array.append(difference)
-    return averaged_array
 
 
